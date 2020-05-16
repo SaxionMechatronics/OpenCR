f633e1269b0c22a60f8058ed9b843d3985c02ec4
321thijs123 <thijs.hof@home.nl>
2020-05-16 23:57:26 +0200

    Added missing advertise call

--- arduino/opencr_arduino/opencr/libraries/turtlebot3/examples/turtlebot3_waffle/turtlebot3_core/turtlebot3_core.ino
+++ arduino/opencr_arduino/opencr/libraries/turtlebot3/examples/turtlebot3_waffle/turtlebot3_core/turtlebot3_core.ino
@@ -37,6 +37,7 @@ void setup()
   nh.advertise(sensor_state_pub);  
   nh.advertise(version_info_pub);
   nh.advertise(imu_pub);
+  nh.advertise(imu_raw_pub);
   nh.advertise(cmd_vel_rc100_pub);
   nh.advertise(odom_pub);
   nh.advertise(joint_states_pub);
